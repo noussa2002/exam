1e0e1161d5f551d4087e98e1fad78d04a7843589
noussa <noussaibanassiri2002@gmail.com>
2024-10-28 10:20:02 +0100

    Internationalisation des messages v3

--- src/main/java/com/example/exam/EmployeeRepository.java
+++ src/main/java/com/example/exam/EmployeeRepository.java
@@ -2,8 +2,10 @@ package com.example.exam;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository  extends JpaRepository<Employee, Integer> {
-    Employee findById(long id);
+    Optional<Employee> findById(Long id);
     void deleteById(Long id);
 
 }
